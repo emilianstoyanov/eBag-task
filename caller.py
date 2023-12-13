b5f6e932926bd53f918839b7a737e75fc07ac621
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-12-13 19:43:43 +0200

    adding a script to automatically fill the fields in the database to a given model

---
+++ caller.py
@@ -0,0 +1,12 @@
+import os
+import django
+from django.db.models import F
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "category_tree_project.settings")
+django.setup()
+
+from automatic_recording_db import populate_model_with_data
+from category_tree.models import Category
+
+populate_model_with_data(Category, 200)
