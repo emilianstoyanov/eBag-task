1cfba71e5dc2f893b0230a822610844370a79244
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-12-13 22:12:05 +0200

    added list filter and search fields for Category model

--- category_tree/admin.py
+++ category_tree/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-
 from category_tree.models import Category
 
-
-# Register your models here.
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'image', 'parent']
+    list_filter = ['name', 'description', 'image', 'parent']
+    search_fields = ['name', 'description']
 
