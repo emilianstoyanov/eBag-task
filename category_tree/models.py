3b2e8961e3c01a4b732e538520e03dde7fdeb927
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-12-13 18:23:11 +0200

    create category model

--- category_tree/models.py
+++ category_tree/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='category_images/', blank=True, null=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
