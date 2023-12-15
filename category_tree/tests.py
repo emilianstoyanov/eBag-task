8a835c8fba169f70792177296c4e91718999f35a
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-12-15 11:06:57 +0200

    created unit test for category model

--- category_tree/tests.py
+++ category_tree/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import os
+import django
 
-# Create your tests here.
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "category_tree_project.settings")
+django.setup()
+
+from django.test import TestCase
+from .models import Category
+
+
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name='Test Category',
+            description='Test description',
+            image='test_image.jpg',
+            parent=None
+        )
+
+    def test_category_creation(self):
+        self.assertEqual(self.category.name, 'Test Category')
+        self.assertEqual(self.category.description, 'Test description')
+        self.assertEqual(self.category.image, 'test_image.jpg')
+        self.assertIsNone(self.category.parent)
+
+    def test_category_str_representation(self):
+        self.assertEqual(str(self.category), 'Test Category')
